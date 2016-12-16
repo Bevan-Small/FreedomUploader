9aa43a5c05cc81e4bf45a6d847e09b64ac396481
Bevan Small <bevan.r.small@gmail.com>
2016-12-16 21:16:44 +1300

    Added Id to Submission class, which is now autogenerated when data is set on the Submission object. Added comments and tidied whitespace

--- app/src/main/java/nz/co/smallcode/freedomuploader/MainActivity.java
+++ app/src/main/java/nz/co/smallcode/freedomuploader/MainActivity.java
@@ -20,6 +20,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -28,9 +29,12 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String BRS_LOG_TAG = "BRS Test";
-    public static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 100;
-    public static final int IMAGE_GALLERY_REQUEST = 100;
+    private static final String BRS_LOG_TAG = "BRS Test";
+    private static final String NO_ASSIGNED_COORDINATE = "200";
+    private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 100;
+    private static final int IMAGE_GALLERY_REQUEST = 100;
+    private static final double ZOOM_LEVEL = 11.0;
+
     Submission mSubmission = new Submission();
 
     @Override
@@ -41,13 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*
-     * Gets the phones location and sets the longitude and latitude views to match
-     *
-     */
 
+    /**
+     * Gets the phones location and sets the longitude and latitude views to match
+     * @param view
+     */
     public void setLocationAsCurrent(View view) {
-        // TODO fetch coordinates and display in textviews
+
         Context context = this;
         LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
@@ -66,86 +70,85 @@ public class MainActivity extends AppCompatActivity {
         // If permission granted, fetch location
         if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-
+            // TODO change PASSIVE_PROVIDER to something that forces an update on GPS/network coords
             Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
             long deviceTime = SystemClock.elapsedRealtimeNanos();
             long timestamp = location.getElapsedRealtimeNanos();
 
-
-
-
-            if ((deviceTime-timestamp) < 60000000000L){
-                // Set long and lat as device location
-
-                EditText latitiude = (EditText)findViewById(R.id.editTextLatitude);
+            if ((deviceTime - timestamp) < 60000000000L) {
+                // Set long and lat views as device location
+                EditText latitiude = (EditText) findViewById(R.id.editTextLatitude);
                 latitiude.setText(Double.toString(location.getLatitude()));
 
-
-                EditText longitude = (EditText)findViewById(R.id.editTextLongitude);
+                EditText longitude = (EditText) findViewById(R.id.editTextLongitude);
                 longitude.setText(Double.toString(location.getLongitude()));
 
 
-
             } else {
-                Toast.makeText(this, "Please update your location",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Please update your location", Toast.LENGTH_LONG).show();
             }
 
-
         }
-
     }
 
-    /*
+    /**
      * Checks that all pertinent data has been entered and makes a submission to the firebase
      * database if data looks good
+     * @param view
      */
-
     public void submitDataToDatabase(View view) {
-        // TODO check all data fields filled
-        // TODO enforce lat/long as numbers
-        boolean fieldsFilled = true;
-        boolean outOfBounds = false;
+
+        // Sets variables of mSubmission to their corresponding view values
+        setSubmissionData();
+
+        // Checks that mSubmission variables are good quality
+        boolean dataGood = checkData();
+
+        // TODO remove this code and the associated textview when satisfied with the id generation
+        TextView idView = (TextView)findViewById(R.id.textViewGeneratedID);
+        idView.setText(mSubmission.getId());
+
+        // makes submission to database
+        if (dataGood){
+            Toast.makeText(this,"Successful!",Toast.LENGTH_LONG).show();
+            // TODO submit data to data base
+            // TODO check data isn't in database already
+        }
+    }
+
+
+    /**
+     *  Writes text field data to mSubmission variable
+     */
+    public void setSubmissionData() {
 
         EditText titleView = (EditText) findViewById(R.id.editTextTitle);
         String title = titleView.getText().toString();
-        if (title.equals("")) {
-            fieldsFilled = false;
-        }
+
         EditText descriptionView = (EditText) findViewById(R.id.editTextDescription);
         String description = descriptionView.getText().toString();
-        if (description.equals("")) {
-            fieldsFilled = false;
-        }
+
         RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         int stars = ratingBar.getNumStars();
-        if (stars == 0) {
-            fieldsFilled = false;
-        }
+
         EditText addressView = (EditText) findViewById(R.id.editTextAddress);
         String address = addressView.getText().toString();
-        if (address.equals("")) {
-            address = "noAddress";
-        }
 
         EditText latitudeView = (EditText) findViewById(R.id.editTextLatitude);
         String latText = latitudeView.getText().toString();
+        // If no latitude has been entered, the latitude is assigned an illegal value
         if (latText.equals("")) {
-            latText = latText + "200";
+            latText = NO_ASSIGNED_COORDINATE;
         }
         double latitude = Double.parseDouble(latText);
 
         EditText longitudeView = (EditText) findViewById(R.id.editTextLongitude);
         String longText = longitudeView.getText().toString();
+        // If no longitude has been entered, the latitude is assigned an illegal value
         if (longText.equals("")) {
-            longText = "200";
+            longText = NO_ASSIGNED_COORDINATE;
         }
         double longitude = Double.parseDouble(longText);
-
-        if (latitude == 200 || longitude == 200) {
-            fieldsFilled = false;
-        } else if (latitude > 85.0 || latitude < -85.0 || longitude >= 180 || longitude <= -180) {
-            outOfBounds = true;
-        }
 
         Spinner tag1Spinner = (Spinner) findViewById(R.id.spinnerTag1);
         String tag1 = tag1Spinner.getSelectedItem().toString();
@@ -156,54 +159,93 @@ public class MainActivity extends AppCompatActivity {
         Spinner tag3Spinner = (Spinner) findViewById(R.id.spinnerTag3);
         String tag3 = tag3Spinner.getSelectedItem().toString();
 
-        if (!fieldsFilled) {
-            CharSequence text = "Please fill in all fields";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-            toast.show();
-        } else if (outOfBounds) {
-            CharSequence text = "Longitude must be between -180 and 180 degrees. Latitude must be " +
-                    "between -85 and 85 degrees. Check Google Maps!";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-            toast.show();
-        } else {
-            // TODO present data for review
-            // TODO make a country code lookup table when other countries are included
-            long index = indexCalculator(latitude, longitude);
-            String countryCode = "NZL";
+        // Calculating the "box" the submission fits into
+        long index = indexCalculator(latitude, longitude);
 
-            mSubmission.setDatabaseData(countryCode, index, title, description,
-                    address, stars, tag1, tag2, tag3, longitude, latitude);
+        // TODO make a country code table when another country is added
+        String countryCode = "NZL";
 
-            CharSequence text = "Success";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-            toast.show();
-
-        }
+        mSubmission.setDatabaseData(countryCode, index, title, description,
+                address, stars, tag1, tag2, tag3, longitude, latitude);
     }
 
-    /*
+    /**
+     * Checks data in mSubmission variable is of good enough quality for submission to database
+     * Checks all necessary fields are filled
+     * Subs noAddress for address if field blank
+     * Checks coordinates are in legal bounds
+     * @return
+     */
+    public boolean checkData(){
+        // TODO abstract this away to Submission class?
+
+        boolean dataGood = true;
+        boolean fieldsFilled = true;
+        boolean outOfBounds = false;
+        double latitude = mSubmission.getLatitude();
+        double longitude = mSubmission.getLongitude();
+
+
+        if (mSubmission.getTitle().equals("")) {
+            fieldsFilled = false;
+        }
+
+        if (mSubmission.getDescription().equals("")) {
+            fieldsFilled = false;
+        }
+
+        if (mSubmission.getRating() == 0) {
+            fieldsFilled = false;
+        }
+
+        // If no address has been entered, substitute noAddress
+        if (mSubmission.getAddress().equals("")) {
+            mSubmission.setAddress("noAddress");
+        }
+
+        if (latitude == 200 || longitude == 200) {
+            fieldsFilled = false;
+        } else if (latitude > 85.0 || latitude < -85.0 || longitude >= 180 || longitude <= -180) {
+            outOfBounds = true;
+        }
+
+        if (!fieldsFilled) {
+            Toast.makeText(this,"Please fill in all fields",Toast.LENGTH_LONG).show();
+            dataGood = false;
+        } else if (outOfBounds) {
+            Toast.makeText(this, "Longitude must be between -180 and 180 degrees. Latitude must be " +
+                    "between -85 and 85 degrees. Check Google Maps!", Toast.LENGTH_LONG ).show();
+            dataGood = false;
+        }
+
+        return dataGood;
+    }
+
+    /**
      * Calculates the index of the box that the new submission falls into on the web mercator
      * projection
+     * @param latitude
+     * @param longitude
+     * @return
      */
-
     public long indexCalculator(double latitude, double longitude) {
         // TODO verify this works 16 Dec 2016
-        double zoom = 11.0;
         long boxesPerRow = 1024;
-        double maxIJ = 256.0 * Math.pow(2.0, zoom);
+        double maxIJ = 256.0 * Math.pow(2.0, ZOOM_LEVEL);
 
+        // Convert coords to radians
         double latRadians = latitude * Math.PI / 180.0;
         double longRadians = longitude * Math.PI / 180.0;
 
-        double x = 128.0 / Math.PI * Math.pow(2.0, zoom) * (longRadians + Math.PI);
-        double y = 128.0 / Math.PI * Math.pow(2.0, zoom) * (Math.PI - Math.log(Math.tan((Math.PI / 4.0 + latRadians / 2.0))));
+        // Calculate x/y coordinates for web mercator projection
+        double x = 128.0 / Math.PI * Math.pow(2.0, ZOOM_LEVEL) * (longRadians + Math.PI);
+        double y = 128.0 / Math.PI * Math.pow(2.0, ZOOM_LEVEL) * (Math.PI - Math.log(Math.tan((Math.PI / 4.0 + latRadians / 2.0))));
 
+        // Calculate the column (i) and row (j) for the box the coordinates belong in
         long i = (long) (x * boxesPerRow / maxIJ);
         long j = (long) (y * boxesPerRow / maxIJ);
 
+        // Calculate the index of the box
         long index = i + j * boxesPerRow;
 
         return index;
@@ -215,19 +257,23 @@ public class MainActivity extends AppCompatActivity {
      * save the image uri in the Submission object
      */
 
+    /**
+     * Allows users to pull an image up from the SD card. The image is received in onActivityResult()
+     *
+     * @param view
+     */
     public void selectAnImage(View view) {
 
+        // Send user off to gallery/app that stores images to pick their picture
         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
         File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         String pictureDirectoryPath = pictureDirectory.getPath();
         Uri data = Uri.parse(pictureDirectoryPath);
 
-        // Set data and type (all image types)
+        // Set data and type allowed(all image types)
         photoPickerIntent.setDataAndType(data, "image/*");
 
         startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
-
-
     }
 
     @Override
@@ -243,6 +289,8 @@ public class MainActivity extends AppCompatActivity {
                     inputStream = getContentResolver().openInputStream(imageUri);
                     Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
 
+                    // Setting the submission object's photo as the picked image
+                    mSubmission.setPhoto(imageBitmap);
 
                     // Setting the found picture to the imageview
                     ImageView photoView = (ImageView) findViewById(R.id.imageViewPhoto);
